5a8e1dac3782a14537c06f1207d4bb977cfc4f36
Hob <93816506+HobbesChiong@users.noreply.github.com>
2022-10-08 17:16:47 -0700

    Added bg image to welcome screen and skip button to main menu screen

---
+++ app/src/main/java/ca/cmpt276/cmpt276assignment3/WelcomeScreen.java
@@ -0,0 +1,28 @@
+package ca.cmpt276.cmpt276assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupMainMenuButton();
+    }
+
+    private void setupMainMenuButton() {
+        Button btn = findViewById(R.id.MainMenubtn);
+        btn.setOnClickListener(v -> {
+            Intent intent = MainMenu.makeIntent(WelcomeScreen.this);
+            startActivity(intent);
+        });
+
+    }
+}
